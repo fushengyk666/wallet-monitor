003c02003a373e34c0ce5843850b65e29d7f0426
fusheng <88888888>
2025-01-21 00:54:19 +0800

    完成

--- src/handlers/send-tx-msg-handler.ts
+++ src/handlers/send-tx-msg-handler.ts
@@ -6,18 +6,14 @@ import { TxMessages } from '../messages/tx-message'
 import { NativeParserInterface } from '../types/general-interfaces'
 
 export class SendTransactionMsgHandler {
-  private txMessages: TxMessages
   private tokenPrices: TokenPrices
-  private formatNumbers: FormatNumbers
   constructor(private bot: TelegramBot) {
     this.bot = bot
 
-    this.txMessages = new TxMessages()
     this.tokenPrices = new TokenPrices()
-    this.formatNumbers = new FormatNumbers()
   }
 
-  public async send(message: NativeParserInterface, chatId: string, walletName?: string) {
+  public async send(message: NativeParserInterface, chatId: string,walletName?: string) {
     const tokenToMc = message.type === 'buy' ? message.tokenTransfers.tokenInMint : message.tokenTransfers.tokenOutMint
     const tokenToMcSymbol =
       message.type === 'buy' ? message.tokenTransfers.tokenInSymbol : message.tokenTransfers.tokenOutSymbol
@@ -25,7 +21,6 @@ export class SendTransactionMsgHandler {
     const TX_SUB_MENU = createTxSubMenu(tokenToMcSymbol, tokenToMc)
 
     try {
-      console.log('message', message)
       if (message.platform === 'raydium' || message.platform === 'jupiter') {
         let tokenMarketCap = message.swappedTokenMc
 
@@ -35,22 +30,26 @@ export class SendTransactionMsgHandler {
           tokenMarketCap *= 1000
         }
 
-        const formattedMarketCap = tokenMarketCap ? this.formatNumbers.formatMarketCap(tokenMarketCap) : undefined
+        const formattedMarketCap = tokenMarketCap ? FormatNumbers.formatPrice(tokenMarketCap) : undefined
+        const tokenPrice = message.swappedTokenPrice
 
-        const messageText = this.txMessages.sendTxMessage(message, formattedMarketCap, walletName)
+        const messageText = TxMessages.txMadeMessage(message, formattedMarketCap, walletName)
         return this.bot.sendMessage(chatId, messageText, {
           parse_mode: 'HTML',
           disable_web_page_preview: true,
           reply_markup: TX_SUB_MENU,
         })
       } else if (message.platform === 'pumpfun') {
-        const tokenInfo = await this.tokenPrices.pumpFunTokenInfo(tokenToMc)
-        console.log('tokenInfo', tokenInfo)
-        let tokenMarketCap = tokenInfo?.usd_market_cap
+        // const tokenInfo = await this.tokenPrices.gmgnTokenInfo(tokenToMc)
+        // let tokenMarketCap = tokenInfo?.market_cap
+        // const tokenInfo = await this.tokenPrices.pumpFunTokenInfo(tokenToMc)
+        // let tokenMarketCap = tokenInfo?.usd_market_cap
 
-        const formattedMarketCap = tokenMarketCap ? this.formatNumbers.formatMarketCap(tokenMarketCap) : undefined
+        let tokenMarketCap = message.swappedTokenMc
 
-        const messageText = this.txMessages.sendTxMessage(message, formattedMarketCap, walletName)
+        const formattedMarketCap = tokenMarketCap ? FormatNumbers.formatPrice(tokenMarketCap) : undefined
+
+        const messageText = TxMessages.txMadeMessage(message, formattedMarketCap, walletName)
         return this.bot.sendMessage(chatId, messageText, {
           parse_mode: 'HTML',
           disable_web_page_preview: true,
@@ -58,7 +57,7 @@ export class SendTransactionMsgHandler {
         })
       } else if (message.platform === 'mint_pumpfun') {
         // new!
-        const messageText = this.txMessages.sendMintTokenMessage(message, walletName)
+        const messageText = TxMessages.tokenMintedMessage(message, walletName)
 
         return this.bot.sendMessage(chatId, messageText, {
           parse_mode: 'HTML',
--- src/lib/format-numbers.ts
+++ src/lib/format-numbers.ts
@@ -2,30 +2,37 @@ export class FormatNumbers {
   constructor() {}
 
   public formatTokenAmount(amount: number) {
-    console.log('AMOUNT', amount)
+    // console.log('AMOUNT', amount)
     let scaledAmount: number
 
-    if (amount.toString().length > 10) {
-      scaledAmount = amount / 1e9
-    } else if (amount.toString().length === 10) {
-      scaledAmount = amount / 1e8
-    } else if (amount.toString().length <= 9) {
+    if (amount >= 1e9) {
       scaledAmount = amount / 1e6
+    } else if (amount >= 1e8) {
+      scaledAmount = amount / 1e5
+    } else if (amount >= 1e6) {
+      scaledAmount = amount / 1e3
     } else {
       scaledAmount = amount
     }
-
     // Format the scaled amount with maximum two fraction digits
     return new Intl.NumberFormat('en-US', { maximumFractionDigits: 2, minimumFractionDigits: 2 }).format(scaledAmount)
   }
 
-  public formatMarketCap(value: number): string {
-    if (value >= 1_000_000) {
+  static formatPrice(value: number): string {
+    if (value >= 1_000_000_000) {
+      return `${(value / 1_000_000_000).toFixed(2)}B`
+    } else if (value >= 1_000_000) {
       return `${(value / 1_000_000).toFixed(2)}M`
     } else if (value >= 1_000) {
       return `${(value / 1_000).toFixed(2)}K`
     } else {
       return value.toFixed(2)
     }
+  }
+
+  static formatTokenPrice(price: number): string {
+    return price.toFixed(8).replace(/^(0\.)(0+)(\d+)/, (_, p1, p2, p3) => {
+      return `0.{${p2.length}}${p3}`
+    })
   }
 }
