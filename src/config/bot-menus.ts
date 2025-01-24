72a0f5f008cbbcaee884c466e1d8613c063f29cf
fusheng <88888888>
2025-01-24 14:51:47 +0800

    优化输出

--- src/config/bot-menus.ts
+++ src/config/bot-menus.ts
@@ -9,19 +9,19 @@ export const createTxSubMenu = (tokenSymbol: string, tokenMint: string) => {
           url: `https://t.me/dogeebot_bot?start=rt_17290579723103_${tokenMint}`,
         },
       ],
-      [
-        {
-          text: `🐴 Buy on Trojan: ${tokenSymbol}`,
-          url: `https://t.me/solana_trojanbot?start=r-handicatbt-${tokenMint}`,
-        },
-      ],
-      [
-        { text: `🐶 BonkBot: ${tokenSymbol}`, url: `https://t.me/bonkbot_bot?start=ref_3au54_ca_${tokenMint}` },
-        {
-          text: `🐸 PepeBoost: ${tokenSymbol}`,
-          url: `https://t.me/pepeboost_sol_bot?start=ref_03pbvu_ca_${tokenMint}`,
-        },
-      ],
+      // [
+      //   {
+      //     text: `🐴 Buy on Trojan: ${tokenSymbol}`,
+      //     url: `https://t.me/solana_trojanbot?start=r-handicatbt-${tokenMint}`,
+      //   },
+      // ],
+      // [
+      //   { text: `🐶 BonkBot: ${tokenSymbol}`, url: `https://t.me/bonkbot_bot?start=ref_3au54_ca_${tokenMint}` },
+      //   {
+      //     text: `🐸 PepeBoost: ${tokenSymbol}`,
+      //     url: `https://t.me/pepeboost_sol_bot?start=ref_03pbvu_ca_${tokenMint}`,
+      //   },
+      // ],
     ],
   }
 
